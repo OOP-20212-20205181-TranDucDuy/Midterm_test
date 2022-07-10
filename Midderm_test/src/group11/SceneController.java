d6ed9c5145b6a42a1f6a44b32ab071cd2e30ff5e
TranDuy <103793658+OOP-20212-20205181-TranDucDuy@users.noreply.github.com>
2022-07-10 17:44:26 +0700

    Add files via upload

--- Midderm_test/src/group11/SceneController.java
+++ Midderm_test/src/group11/SceneController.java
@@ -1,9 +1,14 @@
 package group11;
 
 
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
+import java.util.Scanner;
 
 import org.apache.jena.query.ParameterizedSparqlString;
 import org.apache.jena.query.Query;
@@ -11,55 +16,38 @@ import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.RDFNode;
 
+
+import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.*;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
+
 
 public class SceneController {
-	private Stage stage;
-	private Scene scene;
-	private Parent root;
-	public void switchtoWorkingScene(ActionEvent event) throws IOException {
-		root  = FXMLLoader.load(getClass().getResource("WorkingScene.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
-
-//	    @FXML
-//	    void 8d1fb8(ActionEvent event) {
-//
-//	    }
-//
-//	    @FXML
-//	    void 8d1fb8(ActionEvent event) {
-//
-//	    }
+	private QueryExecution x;
+	private ResultSet results;
+	private List<String> columnNames;
 	    @FXML
 	    private ScrollPane table;
 	    @FXML
 	    private VBox box;
 	    @FXML
 	    private WebView browser;
-	    public void display(ActionEvent event) {
+	    @SuppressWarnings("deprecation")
+		public void display(ActionEvent event) {
 	    	try {
 	    		  
 	    		ParameterizedSparqlString queryStr = new ParameterizedSparqlString();
@@ -83,9 +71,9 @@ public class SceneController {
 	                    "LIMIT 100");
 
 	            Query query = queryStr.asQuery();
-	    		QueryExecution x = QueryExecutionFactory.sparqlService(location,query);
-	            ResultSet results = x.execSelect() ;
-	            List<String> columnNames = results.getResultVars();
+	    		x = QueryExecutionFactory.sparqlService(location, query);
+	            results = x.execSelect() ;
+	            columnNames = results.getResultVars();
 	            //Data get
 	            for (; results.hasNext() ; )
 	            {
@@ -113,13 +101,34 @@ public class SceneController {
 	                   }
 	                }
 	                box.getChildren().add(displayData);
+	               
 	            }
 	        }
 	        catch (Exception e) {
 	  
 	            System.out.println(e.getMessage());
 	        }
-
 	}
-	    
+	    @FXML
+	    private Button save_button;
+	    @FXML
+	    void savefile(ActionEvent event) throws IOException {
+	    	EventHandler<ActionEvent> save_event = new EventHandler<ActionEvent>() {
+	            public void handle(ActionEvent e)
+	            {
+	            	String pathName="D:\\20212\\midtermjava\\";
+	    	        File file = new File(pathName+"dataset.nt");
+	    	        try {
+						file.createNewFile();
+						OutputStream outputStream = new FileOutputStream(file);
+						Model model =x.execConstruct();
+		    	        model.write(outputStream, "TURTLE");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	            }
+	        };
+	    	save_button.setOnAction(save_event);
+	    }
 }
